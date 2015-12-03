da90a9c275157bc587dfabc40b6a6791a6c094a0
DemahomGitHub <demahom.toure@gmail.com>
2015-12-03 19:22:32 +0100

    Projet Java Web
    Page d'accueil et de connexion fait... internationalisation OK

---
+++ Biblios-war/src/java/managedBean/InternationalizationMB.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBean;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.Locale;
+
+/**
+ *
+ * @author Demahom
+ */
+@Named(value = "internationalizationMB")
+@SessionScoped
+public class InternationalizationMB implements Serializable {
+
+    /**
+     * Creates a new instance of InternationalizationMB
+     */
+    public InternationalizationMB() {
+    }
+
+    public Locale getLocal() {
+        return local;
+    }
+    
+    public void setToFrench() {
+        this.setLocal(new Locale("fr"));
+    }
+    
+    public void setToEnglish() {
+        this.setLocal(new Locale("en"));
+    }
+    public void setLocal(Locale local) {
+        this.local = local;
+    }
+    private Locale local = new Locale("en");
+}
